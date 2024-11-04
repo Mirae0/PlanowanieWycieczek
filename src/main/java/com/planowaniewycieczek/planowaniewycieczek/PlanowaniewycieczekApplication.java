d4b7245d0211e2b5ebf3490e1beec9ddfaf866f5
Mirae <162375560+Mirae0@users.noreply.github.com>
2024-11-04 11:49:30 +0100

    Logowanie i rejestracja, działa. Po wejściu na serwer jest napis Planowanie Wycieczek i przyciski logowania i rejestracji, które przenoszą do formularzy rejestracji.

---
+++ src/main/java/com/planowaniewycieczek/planowaniewycieczek/PlanowaniewycieczekApplication.java
@@ -0,0 +1,13 @@
+package com.planowaniewycieczek.planowaniewycieczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanowaniewycieczekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanowaniewycieczekApplication.class, args);
+    }
+
+}
