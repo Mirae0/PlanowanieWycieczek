4765dd8db560d11459be00304a603be1f4d3d1e1
Mirae <162375560+Mirae0@users.noreply.github.com>
2024-11-11 16:21:22 +0100

    Baza danych dla logowania i rejestracji
    Baza danych działa, po resetowaniu serwera zostaja usuniete dane. Baza danych H2 wbudowana dla SpringBoota.

--- src/main/java/com/planowaniewycieczek/planowaniewycieczek/user/UserController.java
+++ src/main/java/com/planowaniewycieczek/planowaniewycieczek/user/UserController.java
@@ -1,9 +1,7 @@
 package com.planowaniewycieczek.planowaniewycieczek.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,11 @@ public class UserController {
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.saveUser(user);
     }
 
 
