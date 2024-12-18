d86e45a1da7bc381bb4115b7c9df6bcbff2f3737
Mirae <162375560+Mirae0@users.noreply.github.com>
2024-12-18 22:40:31 +0100

    Wycieczki v1
    nie ma jeszcze zapisu wycieczek do bazy tylko kolumny sa stworzone, jest formularz wyboru skad, dokad, data, wstepna mapa, wstepny wyglad zakladki wycieczki

--- src/main/java/com/planowaniewycieczek/planowaniewycieczek/user/UserService.java
+++ src/main/java/com/planowaniewycieczek/planowaniewycieczek/user/UserService.java
@@ -1,5 +1,6 @@
 package com.planowaniewycieczek.planowaniewycieczek.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,9 +8,23 @@ import java.util.List;
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public List<User> getUsers(){
-        return List.of(
-                new User("Jan")
-        );
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
     }
 }
